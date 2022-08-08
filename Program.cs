80cdb24e0bbbe771ceafccf190729d0bcf1c9dff
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-08-08 18:45:02 +0300

    Add code

--- Program.cs
+++ Program.cs
@@ -2,6 +2,61 @@
 // Основная задача
 // Печать массива.
 
-Console.Write("Введите размер массива n: ");
-int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер массива: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+string[] inputArray = new string[count];
+for (int i = 0; i < inputArray.Length; i++)
+{
+    Console.WriteLine($"Введите любое слово или набор символов {count} раз(а) + ENTER: ");
+    inputArray[i] = Console.ReadLine();
+}
+
+string PrintArray(string[] array)
+{
+    string txt = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        txt += $"{array[i]} ";
+    }
+    return $"[{txt}]";
+}
+Console.WriteLine($"InputArray = {PrintArray(inputArray)}");
+
+string[] MainTask(string[] inputArray)
+{
+    string tmp = String.Empty;
+    int countOutPutArray = 0;
+    int index = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        tmp = inputArray[i];
+        if (tmp.Length <= 3)
+        {
+            countOutPutArray++;
+        }
+    }
+
+    tmp = String.Empty;
+    string[] outputArray = new string[countOutPutArray];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        tmp = inputArray[i];
+        if (tmp.Length <= 3)
+        {
+            outputArray[index] = inputArray[i];
+            index ++;
+        }
+    }
+    return outputArray;
+}
+string[] result = MainTask(inputArray);
+
+Console.WriteLine($"Result Array = {PrintArray(result)}");
+
+
+
+
 
